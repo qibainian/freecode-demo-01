c899f3b7eb8a201936ef6689b3df16da33b5adc5
qibainian <54347940+qibainian@users.noreply.github.com>
2021-05-01 22:42:11 +0800

    Create bundle.js

---
+++ TributePage/js/bundle.js
@@ -0,0 +1,77 @@
+body {
+    font-size: 1.6rem;
+    line-height: 1.5;
+    text-align: center;
+    color: #333;
+    margin: 0;
+  }
+h1 {
+    font-size: 3.8rem;
+    margin-bottom: 0;
+  }
+@media (max-width: 460px) {
+    h1 {
+        font-size: 3.5rem;
+        line-height: 1;
+        }
+    #main {
+        margin: 0;
+        }
+    #img-caption {
+            font-size: 1.4rem;
+        }
+} 
+h3 {
+    font-size: 1.4rem;
+}
+
+a {
+    color: #477ca7;
+}
+
+a:visited {
+    color: #74638f;
+}
+
+#main {
+    margin: 20px 8px;
+    padding: 10px;
+    border-radius: 5px;
+    background:rgb(236, 236, 236);
+}
+
+img {
+    max-width: 100%;
+    display: block;
+    height: auto;
+    margin: 0 auto;
+}
+
+#img-div {
+    background: white;
+    padding: 10px;
+    margin: 0;
+}
+
+#img-caption {
+    margin: 15px 0 5px 0;
+}
+
+#headline {
+    margin: 50px 0;
+    text-align: center;
+}
+
+ul {
+    max-width: 550px;
+    margin: 0 auto 50px auto;
+    text-align: left;
+    line-height: 1.6;
+}
+
+li {
+    margin: 16px 0;
+    font-size: 1rem;
+}
+
+  
